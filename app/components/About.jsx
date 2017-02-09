ba60a2ec5f2898a61a17a6ab11c6fe1d7745836a
tjdalsvndn9 <ssuyi@ucdavis.edu>
2017-02-09 15:14:06 -0800

    update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 
 var About = (props) => {
   return(
-      <h3>About Component</h3>
+      <div>
+        <h3>About</h3>
+        <p>Welcome to our About page</p>
+      </div>
+
   )
 }
 

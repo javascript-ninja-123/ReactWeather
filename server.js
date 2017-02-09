85e416d165a8f7252c335b6482040a818da329f4
tjdalsvndn9 <ssuyi@ucdavis.edu>
2017-02-09 14:54:48 -0800

    added

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-if(rq.headers['x-forwarded-proto'] === 'http'){
-next();
+if(rq.headers['x-forwarded-proto'] === 'https'){
+	res.redirect('http://' + req.hostname + req.url);
 }
 else{
-	res.redirect('http://' + req.hostname + rq.url);
+	next();
 }
 });
 app.use(express.static('public'));

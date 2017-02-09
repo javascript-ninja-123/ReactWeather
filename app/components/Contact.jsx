0d713549507f7ca5e7d9cbfa25cb0a62b7611fc4
tjdalsvndn9 <ssuyi@ucdavis.edu>
2017-02-09 15:07:39 -0800

    Rename example page

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Contact = (props) =>{
   return(
       <div>
-            <h3>Contact Component!</h3>
+            <h3>Contact</h3>
             <p>Welcome to examples page!</p>
       </div>
 

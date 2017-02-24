74e8795a0ff49bc212a034cb0a5c44f3009f77aa
tjdalsvndn9 <ssuyi@ucdavis.edu>
2017-02-24 10:40:21 -0800

    foundation grid system

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Contact = (props) =>{
   return(
       <div>
-            <h3>Contact</h3>
-            <p>Welcome to examples page!</p>
+            <h1 className="text-center">Example</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+              <li>
+                <Link to='/?location=Philadelphia'>Philadelphia,PA</Link>
+              </li>
+              <li>
+                <Link to='/?location=Rio'>Rio, Brazil</Link>
+              </li>
+            </ol>
       </div>
 
   )
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -15,7 +15,11 @@ var Main = (props)=>{
   return(
     <div>
       <Nav/>
-      {props.children}
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+            {props.children}
+        </div>
+      </div>
     </div>
   );
 }

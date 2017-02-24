d427ea83dd5713a516595625dd876238f725916a
tjdalsvndn9 <ssuyi@ucdavis.edu>
2017-02-24 10:10:12 -0800

    foundation nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -16,12 +16,25 @@ var {Link,IndexLink} = require('react-router');
 
 var Nav = (props) =>{
   return(
-    <div>
-      <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to='about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to ='contact' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Contact</Link>
+      <div className='top-bar'>
+          <div className='top-bar-left'>
+            <ul className='menu'>
+              <li className="menu-text">React Weather App</li>
+              <li>
+                <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              </li>
+              <li>
+                <Link to='about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
+              </li>
+              <li>
+                <Link to ='contact' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
 
-    </div>
+          </div>
+      </div>
   );
 }
 
